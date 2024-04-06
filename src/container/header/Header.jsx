a3d4533324ef21cb62b1f213da2fec3c3fde4b85
MaaX59 <maax.strandberg@gmail.com>
2024-04-06 20:27:06 +0200

    header started

--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 import "./Header.css";
+import bg from "../../images/city-bg.png";
+import car from "../../images/car1.png";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="app__header">
+      <img src={bg} alt="background" />
+      <div className="app__header_navbar"></div>
+      <div className="app__header_content">
+        <div className="app__header_content_text">
+          <h5>For your next trip</h5>
+          <h1>
+            Don´t choose a car <br />
+            Choose the car
+          </h1>
+          <span>
+            Rent the car of your dreams. Unbeatable prices, flexible pick-up
+            options and much more.
+          </span>
+        </div>
+        <div className="app__header_content_image">
+          <img src={car} alt="red car" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
