61c93a83338c541597bc7e4a04112f7f87078830
MaaX59 <maax.strandberg@gmail.com>
2024-04-07 20:35:19 +0200

    hader done, making it scale

--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -13,15 +13,23 @@ const Header = () => {
           <h5>For your next trip</h5>
           <h1>
             Don´t choose a car <br />
-            Choose the car
+            Choose <span>the</span> car
           </h1>
-          <span>
-            Rent the car of your dreams. Unbeatable prices, flexible pick-up
-            options and much more.
-          </span>
+          <h6>
+            {/* Rent the car of your dreams.*/} Unbeatable prices, flexible
+            pick-up options and much more.
+          </h6>
+          <div className="app__header_content_text_buttons">
+            <a href="#awdd" className="button-book">
+              Book Now
+            </a>
+            <a className="button-learn" href="#awdd">
+              Learn More
+            </a>
+          </div>
         </div>
         <div className="app__header_content_image">
-          <img src={car} alt="red car" />
+          <img className="header_img" src={car} alt="red car" />
         </div>
       </div>
     </div>
